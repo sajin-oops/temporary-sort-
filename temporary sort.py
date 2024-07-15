bc660d65a052a811a4a3e44b6f24a0740f9319ac
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-15 19:33:53 +0530

    Add files via upload

---
+++ temporary sort.py
@@ -0,0 +1,11 @@
+city = ["Chennai","Mumbai","Delhi","Maharashtra","Coimbatore","Trichy","Chennai",]
+values = 2,5,3,7,5,8,9
+print(city)
+print(values)
+print(sorted(city)) #this sorted method is temporary only - ascending order
+# output - ['Chennai', 'Chennai', 'Coimbatore', 'Delhi', 'Maharashtra', 'Mumbai', 'Trichy']
+print(city)
+ #output - ['Chennai', 'Mumbai', 'Delhi', 'Maharashtra', 'Coimbatore', 'Trichy', 'Chennai']
+ # the output will change after the first execution
+print(sorted(values)) #output - [2, 3, 5, 5, 7, 8, 9]
+print(values) #output - (2, 5, 3, 7, 5, 8, 9)
